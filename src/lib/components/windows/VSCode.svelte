f5c87ca16929c66d18257d3bdab39d00251a06fd
StamTsag <shadowrlrs@gmail.com>
2025-03-23 11:45:20 +0200

    Remove TODO

--- src/lib/components/windows/VSCode.svelte
+++ src/lib/components/windows/VSCode.svelte
@@ -1,4 +1,3 @@
-<!-- TODO: stackblitz of this project once published -->
 <iframe
 	src={'https://stackblitz.com/github/stamtsag/acrylica?embed=1&file=src%2Flib%2Fcomponents%2FWindow.svelte'}
 	title="Stackblitz"
