c5b790a922708e5f0471d06558a00139f9ab294c
StamTsag <shadowrlrs@gmail.com>
2025-03-23 11:45:10 +0200

    Update vscode

--- src/lib/components/windows/VSCode.svelte
+++ src/lib/components/windows/VSCode.svelte
@@ -1,6 +1,6 @@
 <!-- TODO: stackblitz of this project once published -->
 <iframe
-	src={'https://stackblitz.com/~/github.com/StamTsag/acrylica?embed=1&file=src/lib/components/Desktop.svelte'}
+	src={'https://stackblitz.com/github/stamtsag/acrylica?embed=1&file=src%2Flib%2Fcomponents%2FWindow.svelte'}
 	title="Stackblitz"
 	class="h-screen w-[75vw]"
 ></iframe>
