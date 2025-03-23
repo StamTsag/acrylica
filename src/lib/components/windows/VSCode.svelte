f9c4c92004e87291b4a411dbee144719907ac9c0
StamTsag <shadowrlrs@gmail.com>
2025-03-23 11:30:49 +0200

    Update vscode

--- src/lib/components/windows/VSCode.svelte
+++ src/lib/components/windows/VSCode.svelte
@@ -1,6 +1,6 @@
 <!-- TODO: stackblitz of this project once published -->
 <iframe
 	src={'https://stackblitz.com/~/github.com/StamTsag/acrylica?embed=1&file=src/lib/components/Desktop.svelte'}
-	title="Brave Browser"
+	title="Stackblitz"
 	class="h-screen w-[75vw]"
 ></iframe>
