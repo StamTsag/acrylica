a7ff635877d8cc34356fc51cec5ca129efa88717
StamTsag <shadowrlrs@gmail.com>
2025-03-29 00:31:12 +0200

    format

--- src/runes.svelte.ts
+++ src/runes.svelte.ts
@@ -1,5 +1,5 @@
 import type { App } from './types';
 
-export let loading = $state({value: true});
-export let wallpaper = $state({value: '/wallpapers/0.jpg'});
-export let apps: {value: App[]} = $state({value: []});
+export let loading = $state({ value: true });
+export let wallpaper = $state({ value: '/wallpapers/0.jpg' });
+export let apps: { value: App[] } = $state({ value: [] });
